f162791c11d202a63ba834101fdbdaa7816a3a8a
fridalindstrom <frida_lindstrom_@hotmail.com>
2021-01-29 13:45:29 +0000

    feat:Created profile page and the reroute to it after registration/login

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("You have been succesfully registered!")
+        return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
 
@@ -58,6 +59,7 @@ def login():
             if check_password_hash(existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username = session["user"]))
 
             else:
                 flash("Sorry, username and/or password is incorrect")
@@ -68,6 +70,14 @@ def login():
             return redirect(url_for('login'))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
 
 
 if __name__ == "__main__":
