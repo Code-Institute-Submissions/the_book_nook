6e7bff3318a7f1599a7c64be4ef36a454b7747eb
fridalindstrom <frida_lindstrom_@hotmail.com>
2021-01-29 11:09:06 +0000

    feat: added pymongo and connection to mongo from app.py, created first html-doc

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-import os 
-from flask import Flask
+import os
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -7,10 +10,19 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
 def home():
-    return "hello there"
+    books = mongo.db.books.find()
+    return render_template("home.html", books = books)
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port = int(os.environ.get("PORT")), debug=True)
+    app.run(host=os.environ.get("IP"), port=int(
+        os.environ.get("PORT")), debug=True)
