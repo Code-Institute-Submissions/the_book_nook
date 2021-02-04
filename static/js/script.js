a558e87367e3a2b1c58beac245c1ef3fecf007a1
fridalindstrom <frida_lindstrom_@hotmail.com>
2021-02-04 19:14:31 +0000

    feat: Added fadeout-effect in js for flash messages

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,11 @@
-  $(document).ready(function () {
+  $(document).ready(function () { 
       $(".like").click(function(){
       $(this).toggleClass("heart");
       });
   });
+
+  $(document).ready(function() {
+setTimeout(function() {
+    $('.flash').fadeOut('slow');
+}, 2000);
+});
