e61b2215e3926e98b47bc004c07f844ae0a1984f
fridalindstrom <frida_lindstrom_@hotmail.com>
2021-02-09 14:52:32 +0000

    style:added media queries for nav and footer in base.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,27 @@
-  $(document).ready(function () { 
-      $(".like").click(function(){
-      $(this).toggleClass("heart");
-      });
-  });
-
-  $(document).ready(function() {
-setTimeout(function() {
-    $('.flash').fadeOut('slow');
-}, 2000);
+$(document).ready(function () {
+    $(".like").click(function () {
+        $(this).toggleClass("heart");
+    });
 });
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $('.flash').fadeOut('slow');
+    }, 2000);
+
+
+    $(".burger-menu").click(function () {
+        $(".navigation-list").slideToggle(2000);
+    });
+
+
+    $(".navigation-list li a").click(function () {
+        if ($(window).width() <= 800) {
+            $(".navigation-list").css("display", "none");
+        }
+        else {
+            $(".navigation-list").show();
+        }
+    });
+});
+
